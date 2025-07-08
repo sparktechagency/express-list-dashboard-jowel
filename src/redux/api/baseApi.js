53d767420e4746d406193b10a382974bb48e68af
MdJowelAhmed <mdjowelahmed924@gmail.com>
2025-07-08 14:22:25 +0600

    update base url

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://10.10.7.46:5006/api/v1",
+    baseUrl: "http://75.119.138.163:4173/api/v1",
     prepareHeaders: (headers) => {
       headers.set("ngrok-skip-browser-warning", "true");
       const token = localStorage.getItem("token");
@@ -15,4 +15,6 @@ export const api = createApi({
   }),
   endpoints: () => ({}),
 });
-export const imageUrl = "http://10.10.7.46:5006";
+export const imageUrl = "http://75.119.138.163:4173";
+
+// http://75.119.138.163:4173/auth/login
